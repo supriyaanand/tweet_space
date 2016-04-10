c5bdc5852bf1840f33282fdbafe1ce8ed3bf4598
supriyaanand <supriya.anandrao@gmail.com>
2016-04-10 13:38:01 -0700

    User auth, index, flash and navigation links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+
+  def bootstrap_class_for(flash_type)
+    types = { "success" => "alert-success", "error" => "alert-danger", "alert" => "alert-warning", "notice" => "alert-warning" }
+    types[flash_type]
+  end
+
+  def flash_messages(opts = {})
+    flash.each do |msg_type, message|
+      concat(content_tag(:div, message, class: "alert #{bootstrap_class_for(msg_type)} fade in") do
+               concat content_tag(:button, 'x', class: "close", data: { dismiss: 'alert' })
+               concat message
+             end)
+    end
+    nil
+  end
 end
